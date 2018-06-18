b787a364b07dd235880627cc50c691cc32c9223d
Kokchix <sedoff-online@yandex.ru>
2018-06-19 01:04:42 +0700

    greeting

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export const greetingUser = () => {
+    const userName = readlineSync.question('May I have your name?: ');
+    console.log(`Hey, ${userName}!`);
+};
+
