8224941b2dd5cd865f547e5c6ed8083e6596a2b5
Kokchix <sedoff-online@yandex.ru>
2018-06-25 11:05:31 +0700

    brain-prime bug fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,11 +5,11 @@ import generateNumber from '../utils';
 const gameDescription = 'Is this number prime?';
 
 const isPrime = (value) => {
-  const borderOfReference = 4;
+  const borderOfReference = 2;
   if (value < borderOfReference) {
-    return true;
+    return false;
   }
-  for (let i = 2; i < value; i += 1) {
+  for (let i = borderOfReference; i < value; i += 1) {
     if (value % i === 0) {
       return false;
     }
@@ -18,7 +18,7 @@ const isPrime = (value) => {
 };
 
 const gamePrime = () => {
-  const question = generateNumber(1, 30);
+  const question = generateNumber(1, 3);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   const gameData = cons(question, correctAnswer);
   return gameData;
