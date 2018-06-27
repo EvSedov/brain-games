33fcf26e6a2d08b6cde39b474763843cffcfb372
Kokchix <sedoff-online@yandex.ru>
2018-06-27 20:43:41 +0700

    brain-prime optimize prime number search

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,10 +6,13 @@ const gameDescription = 'Is this number prime?';
 
 const isPrime = (value) => {
   const borderOfReference = 2;
-  if (value < borderOfReference) {
+  if (value === borderOfReference) {
+    return true;
+  } else if (value < borderOfReference || value % borderOfReference === 0) {
     return false;
   }
-  for (let i = borderOfReference; i < value; i += 1) {
+  const optimizedValue = Math.floor(Math.sqrt(value));
+  for (let i = borderOfReference + 1; i <= optimizedValue; i += 2) {
     if (value % i === 0) {
       return false;
     }
@@ -18,7 +21,7 @@ const isPrime = (value) => {
 };
 
 const gamePrime = () => {
-  const question = generateNumber(1, 30);
+  const question = generateNumber(1, 100);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   const gameData = cons(question, correctAnswer);
   return gameData;
